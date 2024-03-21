158ed4b20bd704e9ee1ba16c015e27ba798d54d5
RealSycoBak <alondavid2006@live.com>
2024-03-20 20:59:54 -0400

    Backend Fix

--- index.js
+++ index.js
@@ -40,21 +40,81 @@ io.on("connection", (socket) => {
     
   })
 
-  socket.on("updateCount", (data) => {
-    //Update JSON file with new count data.
-    try {
-      writeFileSync('count.json', JSON.stringify(data, null, 2), 'utf8');
-      console.log('Data successfully saved to disk');
-    } catch (error) {
-      console.log('An error has occurred ', error);
-    }
-  })
-
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
 
 }); 
+
+function updateValue(data) {
+  try {
+    writeFileSync('count.json', JSON.stringify(data, null, 2), 'utf8');
+    console.log('Data successfully saved to disk');
+  } catch (error) {
+    console.log('An error has occurred ', error);
+  }
+}
+
+const classtimer = setInterval(function() {
+const today = new Date().getDay();
+const now = new Date();
+ if (now.getHours() === 0 && now.getMinutes() === 0) {
+    if ([1, 2, 3].includes(today) && Hebrew > 0) {
+        Hebrew--;
+        updateValue("Hebrew", Hebrew, true);
+    }
+   if ([1, 2, 3, 4].includes(today) && Science > 0) {
+     if (today === 3) {
+         Science -= 2;
+       updateValue("Science", Science, true);
+     } else {
+         Science--;
+        updateValue("Science", Science, true);
+     }
+   }
+   if ([1, 3, 4].includes(today) && History > 0) {
+       History--;
+       updateValue("History", History, true);
+   }
+   if ([2].includes(today) && Israel_Ed > 0) {
+       Israel_Ed--;
+       updateValue("israel_ed", Israel_Ed, true);
+   }
+   if ([1].includes(today) && Selah > 0) {
+       Selah--;
+       updateValue("Selah", Selah, true);
+   }
+   if ([2, 3, 4].includes(today) && English > 0) {
+       English--;
+       updateValue("English", English, true);
+   }
+   if ([1, 2, 3].includes(today) && Maths > 0) {
+       Maths--;
+       updateValue("Math", Maths, true);
+   }
+   if ([1, 2, 3, 4].includes(today) && Talmud > 0) {
+     if (today === 1) {
+          Talmud--;
+        updateValue("Talmud", Talmud, true);
+      } else {
+          Talmud-=2;
+         updateValue("Talmud", Talmud, true);
+      }
+   }
+   if ([1, 2, 4].includes(today) && Chumash > 0) {
+       Chumash--;
+       updateValue("Chumash", Chumash, true);
+   }
+   if ([1, 3].includes(today) && Navi > 0) {
+       Navi--;
+       updateValue("Navi", Navi, true);
+   }
+   if ([1, 2, 3, 4].includes(today) && SchoolDays > 0) {
+        SchoolDays--;
+        updateValue("schooldays", SchoolDays, true);
+    }
+}
+}, 1000 * 60);
 
 
 server.listen(3000, () => {
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -34,37 +34,22 @@ socket.on("sendCount", (data) => {
     Chumash = data.Chumash;
     Navi = data.Navi;
     SchoolDays = data.SchoolDays;
-    updateValue("hebrew", data.Hebrew, false)
-    updateValue("science", data.Science, false)
-    updateValue("history", data.History, false)
-    updateValue("israel_ed", data.Israel_Ed, false)
-    updateValue("selah", data.Selah, false)
-    updateValue("english", data.English, false)
-    updateValue("math", data.Maths, false)
-    updateValue("Talmud", data.Talmud, false)
-    updateValue("chumash", data.Chumash, false)
-    updateValue("navi", data.Navi, false)
-    updateValue("schooldays", data.SchoolDays, false)
+    updateValue("hebrew", data.Hebrew)
+    updateValue("science", data.Science)
+    updateValue("history", data.History)
+    updateValue("israel_ed", data.Israel_Ed)
+    updateValue("selah", data.Selah)
+    updateValue("english", data.English)
+    updateValue("math", data.Maths)
+    updateValue("Talmud", data.Talmud)
+    updateValue("chumash", data.Chumash)
+    updateValue("navi", data.Navi)
+    updateValue("schooldays", data.SchoolDays)
 
 }); 
 
-function updateValue(subject, newValue, send) {
+function updateValue(subject, newValue) {
 document.getElementById(subject.toLowerCase()).textContent = newValue;
-if(send) {
-socket.emit("updateCount", {
-    "Hebrew": Hebrew,
-    "Science": Science,
-    "History": History,
-    "Israel_Ed": Israel_Ed,
-    "Selah": Selah,
-    "English": English,
-    "Maths": Maths,
-    "Talmud": Talmud,
-    "Chumash": Chumash,
-    "Navi": Navi,
-    "SchoolDays": SchoolDays
-});
-}
 }
 
 const deadline = new Date("May 2, 2024 17:00:00").getTime();
@@ -87,64 +72,3 @@ const timer = setInterval(function() {
         document.getElementById("countdown").textContent = "WE ARE FREE";
     }
 }, 1000);
-
-const classtimer = setInterval(function() {
-const today = new Date().getDay();
-const now = new Date();
- if (now.getHours() === 0 && now.getMinutes() === 0) {
-    if ([1, 2, 3].includes(today) && Hebrew > 0) {
-        Hebrew--;
-        updateValue("Hebrew", Hebrew, true);
-    }
-   if ([1, 2, 3, 4].includes(today) && Science > 0) {
-     if (today === 3) {
-         Science -= 2;
-       updateValue("Science", Science, true);
-     } else {
-         Science--;
-        updateValue("Science", Science, true);
-     }
-   }
-   if ([1, 3, 4].includes(today) && History > 0) {
-       History--;
-       updateValue("History", History, true);
-   }
-   if ([2].includes(today) && Israel_Ed > 0) {
-       Israel_Ed--;
-       updateValue("israel_ed", Israel_Ed, true);
-   }
-   if ([1].includes(today) && Selah > 0) {
-       Selah--;
-       updateValue("Selah", Selah, true);
-   }
-   if ([2, 3, 4].includes(today) && English > 0) {
-       English--;
-       updateValue("English", English, true);
-   }
-   if ([1, 2, 3].includes(today) && Maths > 0) {
-       Maths--;
-       updateValue("Math", Maths, true);
-   }
-   if ([1, 2, 3, 4].includes(today) && Talmud > 0) {
-     if (today === 1) {
-          Talmud--;
-        updateValue("Talmud", Talmud, true);
-      } else {
-          Talmud-=2;
-         updateValue("Talmud", Talmud, true);
-      }
-   }
-   if ([1, 2, 4].includes(today) && Chumash > 0) {
-       Chumash--;
-       updateValue("Chumash", Chumash, true);
-   }
-   if ([1, 3].includes(today) && Navi > 0) {
-       Navi--;
-       updateValue("Navi", Navi, true);
-   }
-   if ([1, 2, 3, 4].includes(today) && SchoolDays > 0) {
-        SchoolDays--;
-        updateValue("schooldays", SchoolDays, true);
-    }
-}
-}, 1000 * 60);
